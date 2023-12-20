09ab4cd777d96036415c1345f77bba5d3d4b7053
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2023-12-20 21:02:00 +0530

    update

--- Backend/src/main/java/com/example/employeems/repo/EmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeems.repo;
-
-import com.example.employeems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
-}
